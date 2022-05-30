00df3c082e1bb68f1e26815aad091719a68c4055
krystianMoras <krystian.moras@student.put.poznan.pl>
2022-05-30 13:20:09 +0200

    bar chart, scatter plots

--- data_util.py
+++ data_util.py
@@ -13,10 +13,11 @@ class NutrientInfoRepo():
         df = pd.read_excel("assets/Table Ciqual 2020_ENG_2020 07 07.xls",decimal=",",na_values=["-","traces"]).fillna(0)
         df = df.replace({'< ':'',",":'.'},regex=True)
         df = df.apply(pd.to_numeric,errors="ignore")
+        df = df.rename(columns={'alim_nom_eng':'Name','Energy, N x Jones\' factor, with fibres (kcal/100g)':'kcal/100g'})
         df['other sugars'] = (df['Sugars (g/100g)'] - df[['fructose (g/100g)', 'galactose (g/100g)', 'glucose (g/100g)','lactose (g/100g)', 'maltose (g/100g)', 'sucrose (g/100g)']].sum(axis=1)).apply(lambda x : x if x > 0 else 0)
 
 
-        df['other carbs'] = (df['Carbohydrate (g/100g)'] - df[['Sugars (g/100g)','Starch (g/100g)','Fibres (g/100g)', 'Polyols (g/100g)']].sum(axis=1)).apply(lambda x : x if x > 0 else 0)
+        df['other carbs'] = (df['Carbohydrate (g/100g)'] - df[['Sugars (g/100g)','Starch (g/100g)','Fibres (g/100g)']].sum(axis=1)).apply(lambda x : x if x > 0 else 0)
 
 
         df['other fats'] = (df['Fat (g/100g)'] - df[['FA saturated (g/100g)', 'FA mono (g/100g)', 'FA poly (g/100g)']].sum(axis=1)).apply(lambda x : x if x > 0 else 0)
@@ -27,25 +28,79 @@ class NutrientInfoRepo():
         df["Fat kcal"] = df['Fat (g/100g)'] * 9
         df["Alcohol kcal"] = df['Alcohol (g/100g)'] * 7
 
-        # columns_to_drop = ['alim_grp_code', 'alim_ssgrp_code', 'alim_ssssgrp_code',
-        #            'alim_grp_nom_eng', 'alim_ssgrp_nom_eng', 'alim_ssssgrp_nom_eng',
-        #            'alim_nom_sci', 'Energy, Regulation EU No 1169/2011 (kJ/100g)',
-        #            'Energy, N x Jones\' factor, with fibres (kJ/100g)',
-        #            'Energy, N x Jones\' factor, with fibres (kcal/100g)',
-        #            'Protein, crude, N x 6.25 (g/100g)']
-        # df = df.drop(columns_to_drop, axis=1)
-
         return df
 
     def _get_row(self,selected_row):
         return self.df.iloc[selected_row[0]] if selected_row else self.df.iloc[0]
     
+    def get_dropdown_options(self):
+        return ['kcal/100g','Protein (g/100g)',
+       'Carbohydrate (g/100g)', 'Fat (g/100g)', 'Sugars (g/100g)',
+       'fructose (g/100g)', 'galactose (g/100g)', 'glucose (g/100g)',
+       'lactose (g/100g)', 'maltose (g/100g)', 'sucrose (g/100g)',
+       'Starch (g/100g)', 'Fibres (g/100g)', 'Polyols (g/100g)',
+       'Ash (g/100g)', 'Alcohol (g/100g)', 'Organic acids (g/100g)',
+       'FA saturated (g/100g)', 'FA mono (g/100g)', 'FA poly (g/100g)',
+       'FA 4:0 (g/100g)', 'FA 6:0 (g/100g)', 'FA 8:0 (g/100g)',
+       'FA 10:0 (g/100g)', 'FA 12:0 (g/100g)', 'FA 14:0 (g/100g)',
+       'FA 16:0 (g/100g)', 'FA 18:0 (g/100g)', 'FA 18:1 n-9 cis (g/100g)',
+       'FA 18:2 9c,12c (n-6) (g/100g)', 'FA 18:3 c9,c12,c15 (n-3) (g/100g)',
+       'FA 20:4 5c,8c,11c,14c (n-6) (g/100g)',
+       'FA 20:5 5c,8c,11c,14c,17c (n-3) EPA (g/100g)',
+       'FA 22:6 4c,7c,10c,13c,16c,19c (n-3) DHA (g/100g)',
+       'Cholesterol (mg/100g)', 'Salt (g/100g)', 'Calcium (mg/100g)',
+       'Chloride (mg/100g)', 'Copper (mg/100g)', 'Iron (mg/100g)',
+       'Iodine (µg/100g)', 'Magnesium (mg/100g)', 'Manganese (mg/100g)',
+       'Phosphorus (mg/100g)', 'Potassium (mg/100g)', 'Selenium (µg/100g)',
+       'Sodium (mg/100g)', 'Zinc (mg/100g)', 'Retinol (µg/100g)',
+       'Beta-carotene (µg/100g)', 'Vitamin D (µg/100g)', 'Vitamin E (mg/100g)',
+       'Vitamin K1 (µg/100g)', 'Vitamin K2 (µg/100g)', 'Vitamin C (mg/100g)',
+       'Vitamin B1 or Thiamin (mg/100g)', 'Vitamin B2 or Riboflavin (mg/100g)',
+       'Vitamin B3 or Niacin (mg/100g)',
+       'Vitamin B5 or Pantothenic acid (mg/100g)', 'Vitamin B6 (mg/100g)',
+       'Vitamin B9 or Folate (µg/100g)', 'Vitamin B12 (µg/100g)']
     def get_table(self):
 
-        columns_to_pick = ['alim_nom_eng','Carbohydrate (g/100g)','Fat (g/100g)','Protein (g/100g)']
+        columns_to_pick = ['Name','Carbohydrate (g/100g)','Fat (g/100g)','Protein (g/100g)','Cholesterol (mg/100g)', 'Salt (g/100g)', 'Calcium (mg/100g)',
+       'Chloride (mg/100g)', 'Copper (mg/100g)', 'Iron (mg/100g)',
+       'Iodine (µg/100g)', 'Magnesium (mg/100g)', 'Manganese (mg/100g)',
+       'Phosphorus (mg/100g)', 'Potassium (mg/100g)', 'Selenium (µg/100g)',
+       'Sodium (mg/100g)', 'Zinc (mg/100g)', 'Retinol (µg/100g)',
+       'Beta-carotene (µg/100g)', 'Vitamin D (µg/100g)', 'Vitamin E (mg/100g)',
+       'Vitamin K1 (µg/100g)', 'Vitamin K2 (µg/100g)', 'Vitamin C (mg/100g)',
+       'Vitamin B1 or Thiamin (mg/100g)', 'Vitamin B2 or Riboflavin (mg/100g)',
+       'Vitamin B3 or Niacin (mg/100g)',
+       'Vitamin B5 or Pantothenic acid (mg/100g)', 'Vitamin B6 (mg/100g)',
+       'Vitamin B9 or Folate (µg/100g)', 'Vitamin B12 (µg/100g)']
+
         cleaned_df = self.df[columns_to_pick]
 
-        return cleaned_df.to_dict("records")
+        return cleaned_df.to_dict("records"), columns_to_pick
+
+    def get_category_standardized_df(self):
+        return self.df.groupby('alim_grp_nom_eng').mean()
+    
+    def get_rda_chart(self,selected_row):
+        row = self._get_row(selected_row)
+        print(row.keys())
+        rda_values = {
+            'Carbohydrate (g/100g)':130,'Fat (g/100g)':20+35/2,'Protein (g/100g)':56, 'Calcium (mg/100g)':1000,
+       'Copper (mg/100g)':0.9, 'Iron (mg/100g)':8,'Magnesium (mg/100g)':400, 'Manganese (mg/100g)':2.3,
+       'Phosphorus (mg/100g)':700, 'Potassium (mg/100g)':4700, 'Selenium (µg/100g)':55,
+       'Sodium (mg/100g)':2300, 'Zinc (mg/100g)':11, 'Vitamin D (µg/100g)':15, 'Vitamin E (mg/100g)':15, 'Vitamin C (mg/100g)':90,
+       'Vitamin B1 or Thiamin (mg/100g)':1.2, 'Vitamin B2 or Riboflavin (mg/100g)':1.3,
+       'Vitamin B3 or Niacin (mg/100g)':16, 'Vitamin B6 (mg/100g)':1.3, 'Vitamin B12 (µg/100g)':2.4*1000
+        }
+        rda_df = pd.DataFrame(columns=['value','nutrient'])
+        for nutrient in rda_values:
+            print(row[nutrient])
+            print(rda_df.head())
+            row_d = {'nutrient':nutrient,'value':row[nutrient]*100 / rda_values[nutrient]}
+
+            rda_df = pd.concat([rda_df,pd.DataFrame(row_d,index=[0])])
+        
+        return rda_df
+
 
     def get_calorie_info(self,selected_row):
 
@@ -53,7 +108,7 @@ class NutrientInfoRepo():
         
         names = ['Protein kcal','Carbohydrate kcal','Fat kcal','Alcohol kcal']
         values = row[names].to_numpy().transpose().flatten()
-        title = row['alim_nom_eng']
+        title = row['Name']
         return values,names,title
 
 
--- main.py
+++ main.py
@@ -17,26 +17,53 @@ def get_markdown(filename) -> dcc.Markdown:
 
 app = Dash(__name__)
 repo = data_util.NutrientInfoRepo()
-
-app.layout = html.Div([
+table, columns = repo.get_table()
+app.layout = html.Div(
+    [
     get_markdown('markdowns/headnotes.md'),
     get_markdown('markdowns/table.md'),
     
     dash_table.DataTable(
         id="data_table",
-        data=repo.get_table(),
+        data=table,
         columns=[
-            {"name": i, "id": i} for i in  ['alim_nom_eng','Carbohydrate (g/100g)','Fat (g/100g)','Protein (g/100g)']],
-        row_selectable='single'),
+            {"name": i, "id": i} for i in columns],
+        row_selectable='single',
+        page_size=25,
+        filter_action="native",
+        style_data={
+        'whiteSpace': 'normal',
+        'height': 'auto',
+    },),
     get_markdown('markdowns/calorie_chart.md'), 
     dcc.Graph(id="calorie_graph"),
     get_markdown('markdowns/nutrient_composition.md'),
     dcc.Graph(id="nutrient_composition_graph"),
     get_markdown('markdowns/top_nutrients.md'),
+    html.Div(children=[
+       dcc.Dropdown(options=repo.get_dropdown_options(), value=repo.get_dropdown_options()[0],placeholder="Select X axis", id='x-dropdown'),
+        dcc.Dropdown(options=repo.get_dropdown_options(), value=repo.get_dropdown_options()[0],placeholder="Select Y axis", id='y-dropdown'),
+        dcc.Dropdown(options=["None"] + repo.get_dropdown_options(),value= "None",placeholder="Select Z axis", id='z-dropdown'),
+    ]),
+    dcc.Graph(id="nutrient_corelation_scatter"),
     get_markdown('markdowns/nutrients_categories.md'),
+    html.Div(children=[
+       dcc.Dropdown(options=repo.get_dropdown_options(), value=repo.get_dropdown_options()[0],placeholder="Select X axis", id='x-dropdown_2'),
+        dcc.Dropdown(options=repo.get_dropdown_options(), value=repo.get_dropdown_options()[0],placeholder="Select Y axis", id='y-dropdown_2'),
+        dcc.Dropdown(options=["None"] + repo.get_dropdown_options(),value= "None",placeholder="Select Z axis", id='z-dropdown_2'),
+    ]),
+    dcc.Graph(id="nutrient_corelation_grouped_scatter"),
+    
     get_markdown('markdowns/macros_filled.md'),
+    dcc.Input(
+            id="input_mass",
+            type="number",
+            placeholder="input type {}".format("number"),
+        ),
+    dcc.Graph(id="rda_bar_chart"),
     get_markdown('markdowns/footnotes.md')
     ])
+    
 
 @app.callback(
     Output("calorie_graph", "figure"),
@@ -45,7 +72,7 @@ def generate_calorie_chart(selected_row_id):
     
     values,names,title = repo.get_calorie_info(selected_row_id)
     
-    fig = px.pie(values=values, names=names, title=title)
+    fig = px.pie(values=values, names=names, title=title, height=800)
     return fig
 
 @app.callback(
@@ -55,7 +82,7 @@ def generate_nutrient_chart(selected_row_id):
     
     data_frame,path,values = repo.get_nutrient_composition(selected_row_id)
     print(values)
-    fig = px.sunburst(data_frame, path=path, values=values)
+    fig = px.sunburst(data_frame, path=path, values=values, height=800)
     fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
 
     return fig
@@ -67,18 +94,42 @@ def generate_nutrient_chart(selected_row_id):
 #     )
 # def generate_macros_bar_chart(selected_row_id,food_amount_value):
 #     pass
-# @app.callback(
-#     Output("top_foods_by_nutrient_chart", "figure"),
-#    # Input("nutrient_picker",'value/id')
-#    )
-# def generate_top_foods_by_nutrient(selected_row_id):
-#     pass
-# @app.callback(
-#     Output("food_category_nutrient_plot", "figure"),
-#    # Input("nutrient_picker_2",'value/id')
-#    )
-# def generate_food_category_nutrient_plot(selected_row_id):
-#     pass
+@app.callback(
+    Output("nutrient_corelation_scatter", "figure"),
+    Input("x-dropdown",'value'),
+    Input('y-dropdown','value'),
+    Input('z-dropdown','value')
+   )
+def generate_nutrient_corelation_scatter(x_column,y_column, z_column):
+    if z_column=="None":
+        #2d plot
+        return px.scatter(repo.df,x=x_column,y=y_column,hover_name="Name",symbol='alim_grp_nom_eng')
+    else:
+        #3d plot
+        return px.scatter_3d(repo.df,x=x_column,y=y_column,z=z_column,hover_name="Name",symbol='alim_grp_nom_eng')
+    
+@app.callback(
+   Output("nutrient_corelation_grouped_scatter", "figure"),
+    Input("x-dropdown_2",'value'),
+    Input('y-dropdown_2','value'),
+    Input('z-dropdown_2','value')
+   )
+def generate_food_category_nutrient_plot(x_column,y_column, z_column):
+    df = repo.get_category_standardized_df()
+   # print(df.head())
+    if z_column=="None":
+        #2d plot
+        return px.scatter(df,x=x_column,y=y_column,hover_name=df.index)
+    else:
+        #3d plot
+        return px.scatter_3d(df,x=x_column,y=y_column,z=z_column,hover_name=df.index)
+
+@app.callback(
+    Output("rda_bar_chart", "figure"),
+    Input("data_table",'selected_rows'))
+def generate_bar_chart(selected_row_id):
+    rda_df = repo.get_rda_chart(selected_row_id)
+    return px.bar(rda_df,x='nutrient',y='value')
 
 
 if __name__ == '__main__':
